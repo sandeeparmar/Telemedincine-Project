f2b11a05406faa5b16917940ef3a711e11ab3c6b
sandeep parmar <23bit062@ietdavv.edu.in>
2026-06-20 23:05:55 +0530

    critical information hide

--- Frontend/Hospital_Tracker/src/pages/Login.jsx
+++ Frontend/Hospital_Tracker/src/pages/Login.jsx
@@ -6,8 +6,8 @@ import { Link, useNavigate } from "react-router-dom";
 export default function Login() {
   const navigate = useNavigate();
   const { login } = useContext(AuthContext);
-  const [email, setEmail] = useState("23btc193@ietdavv.edu.in");
-  const [password, setPassword] = useState("Password@123");
+  const [email, setEmail] = useState("sandeep@example.com");
+  const [password, setPassword] = useState(".........");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
